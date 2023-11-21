402df4b037efbd46ef0f8da6ed039d50b0e9021e
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-11-21 13:15:03 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import HelloWorld from "./components/HelloWorld";
+import Variables from "./components/Variables";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -26,6 +27,7 @@ function App() {
             </div>
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
             <HelloWorld />
+            <Variables />
         </>
     );
 }
